3e8a1b39104e39816b6c49562d614b2dd214af53
Tomáš Milet <imilet@fit.vutbr.cz>
2026-04-21 13:37:37 +0200

    WINDOW

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 
+#include<SDL3/SDL.h>
+
 int main(int argc,char*argv[]){
-  std::cerr << "HELLO this is MULe" << std::endl;
+
+  auto window = SDL_CreateWindow("MULe",1024,768,0);
+
+  bool running = true;
+  while(running){//main loop
+  
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){//event loop
+      if(event.type == SDL_EVENT_QUIT)running = false;
+    }
+
+
+  }
+
+  SDL_DestroyWindow(window);
   return 0;
 }
